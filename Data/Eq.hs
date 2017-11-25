07937bda39f8c2354545ecd1b062f54f0135049f
AntonXue <antonxue1@gmail.com>
2017-11-25 17:51:32 -0500

    commented everything

--- Data/Eq.hs
+++ Data/Eq.hs
@@ -1,22 +1,22 @@
-{-# LANGUAGE Trustworthy #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-
------------------------------------------------------------------------------
--- |
--- Module      :  Data.Eq
--- Copyright   :  (c) The University of Glasgow 2005
--- License     :  BSD-style (see the file libraries/base/LICENSE)
+-- {-# LANGUAGE Trustworthy #-}
+-- {-# LANGUAGE NoImplicitPrelude #-}
 -- 
--- Maintainer  :  libraries@haskell.org
--- Stability   :  stable
--- Portability :  portable
---
--- Equality
---
------------------------------------------------------------------------------
-
-module Data.Eq (
-   Eq(..),
- ) where
-
-import GHC.Base
+-- -----------------------------------------------------------------------------
+-- -- |
+-- -- Module      :  Data.Eq
+-- -- Copyright   :  (c) The University of Glasgow 2005
+-- -- License     :  BSD-style (see the file libraries/base/LICENSE)
+-- -- 
+-- -- Maintainer  :  libraries@haskell.org
+-- -- Stability   :  stable
+-- -- Portability :  portable
+-- --
+-- -- Equality
+-- --
+-- -----------------------------------------------------------------------------
+-- 
+-- module Data.Eq (
+--    Eq(..),
+--  ) where
+-- 
+-- import GHC.Base
--- GHC/IO/Encoding.hs-boot
+++ GHC/IO/Encoding.hs-boot
@@ -1,10 +1,10 @@
-{-# LANGUAGE Trustworthy #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-
-module GHC.IO.Encoding where
-
-import GHC.IO (IO)
-import GHC.IO.Encoding.Types
-
-getLocaleEncoding, getFileSystemEncoding, getForeignEncoding :: IO TextEncoding
-
+-- {-# LANGUAGE Trustworthy #-}
+-- {-# LANGUAGE NoImplicitPrelude #-}
+-- 
+-- module GHC.IO.Encoding where
+-- 
+-- import GHC.IO (IO)
+-- import GHC.IO.Encoding.Types
+-- 
+-- getLocaleEncoding, getFileSystemEncoding, getForeignEncoding :: IO TextEncoding
+-- 
