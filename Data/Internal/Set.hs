80295ad5de90134a753ab318e654af93d144691e
Bill Hallahan <billthallahan@gmail.com>
2021-05-30 21:25:55 -0400

    More set functions

--- Data/Internal/Set.hs
+++ Data/Internal/Set.hs
@@ -230,7 +230,7 @@ module Data.Set.Internal (
             -- , merge
             ) where
 
--- import Prelude hiding (filter,foldl,foldr,null,map,take,drop,splitAt)
+import Prelude hiding (filter,foldl,foldr,null,map,take,drop,splitAt)
 -- import Control.Applicative (Const(..))
 -- import qualified Data.List as List
 -- import Data.Bits (shiftL, shiftR)
